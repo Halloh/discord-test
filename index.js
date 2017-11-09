b6c43b65527c8c0ee46beb20baccce67a99dd5cb
Alex Villanueva <19585129+Halloh@users.noreply.github.com>
2017-11-09 15:10:26 -0800

    Update anti 2 stock function

--- index.js
+++ index.js
@@ -34,11 +34,32 @@ client.on('message', function (message) {
     }
 });
 
-//Anti 2 stock joke function
-//NOTE: supposedly, sendMessage() is deprecated.  Use something else if possible
+// listener Event:  Message Recieved (this will run every time a message is recieved)
 client.on("message", message => {
-    if(message.content === '2 stock'){
-        message.author.sendMessage("What the fuck did you just fucking say about my master, you little bitch? I’ll have you know I graduated top of my class in Bot School, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over Discord? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo."); 
+
+    //Variables
+    let sender = message.author; // The person who sent the message
+    let msg = message.content.toUpperCase(); // Takes the message, and makes it all upercase
+    let prefix = '>' // The text before commands, you can set this howerver you want
+
+    //Weather command - requires weather.js
+
+
+
+
+
+    //Anti 2 stock joke function
+    if(message.content === '2 stock' || message.content === '2-stock' || message.content === '2stock'){
+
+        //Deleting specific messages (messages that are not an ID for me)
+        if (message.channel.id === '346801330817466368' || message.channel.id === '376477068017532930') //First id is 'general'.  Second id is 'testing'
+        {
+                message.delete() // This deletes message
+                console.log("We went off");
+        }
+
+        //message.edit('This is my new content!');
+        sender.send("What the fuck did you just fucking say about my master, you little bitch? I’ll have you know I graduated top of my class in Bot School, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over Discord? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo."); 
     }
 });
 
